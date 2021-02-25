73c24b84f47fa2e33354f2e760fe93a3be5c43a9
Kerstin Sch <kerstinm185@yahoo.de>
2021-02-25 08:09:30 +0100

    inserted sys.argv

--- photo_prediction.py
+++ photo_prediction.py
@@ -1,6 +1,10 @@
-'''
+"""
 predict a photo from CNN_CIFAR10 model
-'''
+To start prediction:
+python photo_prediction.py 'path & name of pic'
+"""
+
+import sys
 import h5py
 from tensorflow.keras.models import load_model
 import cv2
@@ -13,9 +17,9 @@ category=['airplane','automobile', 'bird','cat','deer','dog','frog','horse',
 code=['0','1','2','3','4','5','6','7','8','9']
 labels = dict(zip(code, category))
 
-img_unsplash=cv2.imread('picture_bird.jpg')
+pic = sys.argv[1]
+img_unsplash=cv2.imread(pic)
 img_unsplash=img_unsplash[50:400,100:550]
-plt.imshow(img_unsplash)
 
 image = cv2.resize(img_unsplash, dsize=(32,32),interpolation=cv2.INTER_CUBIC)
 image=image.reshape(-1,32,32,3)
